f0b83a69943d651cec6e0dbe656e970c9ddd2179
dicklant <dicklant@hotmail.com>
2023-11-18 23:16:17 +0100

    normalize port

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = normalizePort(process.env.PORT || '3000');
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
